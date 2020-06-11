9e9568d154e24f98bec1db7d8584bf6d5834bd30
dalbonio <dalbonio2@gmail.com>
2020-06-11 14:40:46 -0300

    adjusting title and content font size and adding date under title

--- src/pages/example/example.js
+++ src/pages/example/example.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Corona from "../../images/coronavirus-covid-19-15-700x374.webp";
+import Corona4 from "../../images/coronavirus4.jpg"
+import Corona3 from "../../images/corona3.jpg"
 import "./example.css";
 
 const Example = () => {
@@ -7,7 +9,8 @@ const Example = () => {
     <React.Fragment>
       <div className="container">
         <h1><span className="headline">Recomendações sobre o Corona</span></h1>
-        {/*<img src={Corona} />*/}
+        <span className = "postdate">{new Date().toLocaleDateString()}</span>
+        <img className="coronaimg" src={Corona3} />
         <p className="content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a semper
           lectus, scelerisque fermentum lectus. Duis vitae tortor elementum,
